2ca91531bb392cf9ada01ff37b7034f9ba08601f
Paulo Muniz <paulo2ferreira@gmail.com>
2026-07-30 08:30:52 -0300

    Add aula 2

--- aulas_js/aula 1/contador.js
+++ aulas_js/aula 1/contador.js
@@ -1,8 +1,29 @@
 // const, let, var
 let contador = 0;
 
+// aula 1
 function incrementar() {
-    // contador += 1;
-    contador = contador + 1;
-    alert(`O valor é ${contador}.`)
+    contador += 1;
+    document.querySelector('#contador')
+        .innerHTML = contador;
 }
+// chamada da função - com ()
+// passar uma função - sem ()
+
+function colocarIncrementar() {
+    document
+        .querySelector('button')
+        .onclick = incrementar;
+}
+
+document
+    .addEventListener(
+        'DOMContentLoaded',
+        colocarIncrementar
+    );
+// aula 0
+// function incrementar() {
+//     // contador += 1;
+//     contador = contador + 1;
+//     alert(`O valor é ${contador}.`)
+// }
